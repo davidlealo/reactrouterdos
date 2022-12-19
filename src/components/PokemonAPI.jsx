7e049c9d31fac9b2e3dad2c5169abfd8a369f624
davidlealo <davidlealo@gmail.com>
2022-12-18 23:38:16 -0300

    Sitio publiado en https://funny-dodol-0e82c1.netlify.app/

--- src/components/PokemonAPI.jsx
+++ src/components/PokemonAPI.jsx
@@ -4,6 +4,9 @@ function PokemonAPI() {
     <div>
         <h1>Bienvenido maestro Pokemon</h1>
         <img src='https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png' />
+        {
+          //Sitio publicado en https://funny-dodol-0e82c1.netlify.app/
+        }
     </div>
   )
 }
